20b3e0c72167165143e22ca31550409e7ec5616e
WLAN32 <gogoleuser@gmail.com>
2019-06-29 17:59:33 +0200

    2 files modified

--- src/app/sabw-caesar/sabw-caesar.component.ts
+++ src/app/sabw-caesar/sabw-caesar.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SabwCaesarComponent implements OnInit {
  zahl=12;
+
+ dreheScheibe() {
+   console.log("Hallo");
+ }
+
+
   constructor() { }
 
   ngOnInit() {
