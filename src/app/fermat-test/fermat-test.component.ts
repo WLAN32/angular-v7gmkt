ef59b09f7691bc6bd5823e26580263ac3ccc04c3
WLAN32 <gogoleuser@gmail.com>
2019-08-28 11:51:28 +0200

    2 files modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -14,20 +14,31 @@ export class FermatTestComponent implements OnInit {
   fermattest() {
 
     this.ergebnis = 'starte test' + this.versuchskaninchen + '...';
-    let zeuge = 2;
-    //Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
-    let zwischenergebnis = 1;
-    for (let i = 0; i < this.versuchskaninchen - 1; i++) {
-      zwischenergebnis = zwischenergebnis * zeuge;
 
-    }
-    zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
-    if (zwischenergebnis === 1) {
-      this.ergebnis += 'Zeuge ' + zeuge + ' OK... ';
-    } else {
-      this.ergebnis += 'Zeuge' + zeuge + ' NICHT OK';
-    }
+    let endergebnis = true;
 
+    for (let zeuge = 2; zeuge < this.Anzahl + 2; zeuge++) {
+
+
+      //Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
+      let zwischenergebnis = 1;
+      for (let i = 0; i < this.versuchskaninchen - 1; i++) {
+        zwischenergebnis = zwischenergebnis * zeuge; zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
+
+      }
+      if (zwischenergebnis === 1) {
+        this.ergebnis += 'Zeuge ' + zeuge + ' OK... ';
+      } else {
+        this.ergebnis += 'Zeuge' + zeuge + ' NICHT OK';
+        endergebnis = false;
+      }
+    }
+if  (endergebnis === false) {
+  this.ergebnis += 'Test nicht bestanden';
+}
+else {
+  this.ergebnis += 'Test Bestanden';
+}
   }
 
   ngOnInit() {
